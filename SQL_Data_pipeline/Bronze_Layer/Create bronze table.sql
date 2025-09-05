c326fd0c813345df7834689ced1cf0839620d13e
God'spower <66071052+Ihenx@users.noreply.github.com>
2025-09-06 00:42:03 +0100

    Add files via upload
    create bronze layer

---
+++ SQL_Data_pipeline/Bronze_Layer/Create bronze table.sql
@@ -0,0 +1,56 @@
+if OBJECT_ID('bronze.telecom_customer_churn') is not null
+	drop table telecom_customer_churn
+go
+
+create table bronze.telecom_customer_churn(
+Customer_id nvarchar(50) primary key not null,
+Gender varchar(50),
+Age int,
+Married varchar(50),
+Number_of_Department int,
+City varchar(50),
+Zipcode int,
+Latitude float,
+Longitude float,
+Number_of_Referrals int,
+Tenure_in_Months int,
+Offer varchar(50),
+Phone_Service Varchar(50),
+Avg_Monthly_Long_Distant_charges float,
+Multiple_Lines varchar(50),
+Internet_Service varchar(50),
+Internet_Type varchar(50),
+Avg_Monthly_GB_Download int,
+Online_Security varchar(50),
+Online_Backup varchar(50),
+Device_Detection_plan varchar(50),
+Premium_Tech_Support varchar(50),
+Streaming_TV varchar(50),
+Streaming_Movies varchar(50),
+Streaming_Music varchar(50),
+Unlimited_Data varchar(50),
+Contract varchar(50),
+Paperless_Billing varchar(50),
+Payment_Method varchar(50),
+Monthly_Charge float,
+Total_Charges float,
+Total_Refunds float,
+Total_Extra_Charges float,
+Total_Long_Distant_Charges float,
+Total_Revenue float,
+Customer_Status varchar(50),
+Churn_Category varchar(50),
+Churn_Reason varchar(50)
+
+);
+
+if OBJECT_ID('bronze.Telecom_ZipCode_Population', 'U') is not null
+	drop table bronze.Telecom_ZipCode_Population;
+go
+
+create table bronze.Telecom_ZipCode_Population(
+Zipcode int,
+Population int
+);
+
+
---
+++ SQL_Data_pipeline/Bronze_Layer/Load bronze Table.sql
@@ -0,0 +1,68 @@
+create or alter procedure bronze.load_customer_churn as
+begin
+	declare @start_time datetime, @end_time datetime, @batch_startime datetime, @batch_endtime datetime;
+begin try
+	set @batch_startime = GETDATE()
+
+
+	print ' =================================================================================================================='
+	print '======================= Loading  bronze.customer_churn ============================================================'
+	print ' ==================================================================================================================='
+	set @start_time = GETDATE()
+
+	print ' <<< truncate table:bronze.crm_cust_info'
+truncate table  bronze.telecom_customer_churn
+
+bulk insert bronze.telecom_customer_churn
+from 'C:\Users\godsp\Downloads\TELECOM DATASET-20250821T112301Z-1-001\TELECOM DATASET\telecom_customer_churn.csv'
+with(
+	firstrow =2,
+	fieldterminator =',',
+	tablock
+)
+set @end_time = GETDATE()
+
+print' load bronze.customer_churn duration' + cast(datediff(second,@start_time,@end_time) as nvarchar) + ' Seconds'
+set @batch_endtime = GETDATE()
+
+print '<<<<<------------------------------------------------------>>>>>>'
+
+
+
+	print ' =================================================================================================================='
+	print '======================= Loading  bronze.telecom_ZipCode_Population ============================================================'
+	print ' ==================================================================================================================='
+	set @start_time = GETDATE()
+
+print ' <<< truncate table:bronze.crm_cust_info'
+
+truncate table bronze.telecom_ZipCode_Population
+
+bulk insert telecom_ZipCode_Population
+from 'C:\Users\godsp\Downloads\telecom_zipcode_population.csv'
+with (
+		firstrow =2,
+		fieldterminator=',',
+		tablock
+	)
+
+set @end_time = GETDATE()
+
+print' load bronze.customer_churn duration' + cast(datediff(second,@start_time,@end_time) as nvarchar) + ' Seconds'
+
+print '<<<<<------------------------------------------------------>>>>>>'
+end try
+
+Begin catch
+	print '==================================================== '
+	print 'Error During Loading Bronze Layer'
+
+	print 'Error Message: ' + error_message();
+	print 'Error Message: ' + cast(error_number() as nvarchar);
+	print 'Error Message: ' + cast(error_state() as nvarchar);
+	print '==================================================== '
+end catch
+
+end;
+
+exec bronze.load_customer_churn
